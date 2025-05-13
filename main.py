f5a0deb8c9436766b9ce5c7bae36c3cb2505a4c4
suraj sah <surajsah1400@gmail.com>
2025-05-13 23:50:14 +0545

    Corrected the backend call for frontend

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 # CORS setup
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:8000"],  # Replace with frontend domain
+    allow_origins=["http://127.0.0.1:5500"],  # Replace with frontend domain
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
